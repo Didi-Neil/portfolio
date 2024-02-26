f24acd3bfb166f430870c3ed1457210d77f3cffd
DidiNeil <120100612+Didi-Neil@users.noreply.github.com>
2024-02-26 18:43:33 +0100

    Rubik's Cube

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,31 +1,25 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
+import { OrbitControls, Preload, useGLTF, useAnimations, useTexture } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const groupRef = useRef(); // Référence pour le groupe racine
+  const { nodes, animations } = useGLTF("./free__rubiks_cube_3d/scene.gltf");
+  const { actions } = useAnimations(animations, groupRef);
+  //const texture = useTexture("./marching_cubes/textures/Material_baseColor.png");
+
+  useEffect(() => {
+    actions.Animation.play(); // Assurez-vous que le nom 'Animation' correspond au nom réel de votre animation
+  }, [actions]);
 
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
+    <group ref={groupRef} scale={isMobile ? 0.7 : 0.75}>
+      <primitive 
+      object={nodes.Sketchfab_Scene} 
+      //material={<meshStandardMaterial map={texture} />} 
       />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+    </group>
   );
 };
 
@@ -33,43 +27,27 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
+    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
   }, []);
 
   return (
-    <Canvas
-      frameloop='demand'
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
+    <Canvas frameloop='demand' shadows dpr={[1, 2]} camera={{ position: [30, 0, 10], fov: 30 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        <hemisphereLight intensity={0.15} groundColor='black' />
+        <spotLight position={[-20, 50, 50]} angle={0.5} penumbra={1} intensity={1} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
+        <pointLight intensity={2} />
+        <OrbitControls enableZoom={true} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Computers isMobile={isMobile} />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );
